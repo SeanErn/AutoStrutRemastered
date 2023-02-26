ebc73b4bbf2978b6997a00cef92c4681aecb816c
SeanErn <seanernstes@gmail.com>
2023-02-26 01:23:59 -0500

    loading mod

---
+++ AutostrutRemastered/AutostrutRemastered/AutostrutRemasteredMod.cs
@@ -0,0 +1,13 @@
+using SpaceWarp.API.Mods;
+
+namespace AutostrutRemastered
+{
+    [MainMod]
+     public class AutostrutRemasteredMod : Mod
+    {
+        public override void OnInitialized()
+        {
+            Logger.Info("TEST");
+        }
+    }
+}
